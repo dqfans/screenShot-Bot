646872601b45e13abf48dcc0da41521a8fbe43a0
noupus <63485872+noupus@users.noreply.github.com>
2020-06-10 14:06:50 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -551,5 +551,5 @@ def gen_ik_buttons():
             i_keyboard = []
         if i==10:
             btns.append(i_keyboard)
-    btns.append([InlineKeyboardButton('Trim Video!', 'trim')])
+    btns.append([InlineKeyboardButton('✂ Trim Video!', 'trim')])
     return btns
